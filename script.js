5cc2d9af96813ec6a4300d14116c9f3055434c06
James Mackay <jamesrossmackay@gmail.com>
2019-05-27 14:59:08 +0800

    Remote lab component configured

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-document.getElementById("topicId").selectedIndex = "3";
+// Invoke functions
+selectTopic();
+
+// Detect where the user came from to filter topics
+function selectTopic() {
+    // If user came from Remote Labs
+    if (document.referrer == "https://lab.electromeet.com/support") {
+        document.getElementById("topicId").selectedIndex = "2";
+    }
 
 
-document.getElementById("topicId").onchange("click", function(){
-  var data=$(":input[name]","#dynamic-form").serialize();$.ajax("ajax.php/form/help-topic/"+this.value,{data:data,dataType:"json",success:function(a){$("#dynamic-form").empty().append(a.html),$(document.head).append(a.media)}});
-});
+    // Refresh the module with the updated content (Taken from an in-built script)
+    document.getElementById("topicId").onchange("click", function() {
+        var data = $(":input[name]", "#dynamic-form").serialize();
+        $.ajax("ajax.php/form/help-topic/" + this.value, {
+            data: data,
+            dataType: "json",
+            success: function(a) {
+                $("#dynamic-form").empty().append(a.html),
+                $(document.head).append(a.media)
+            }
+        });
+    });
+}
