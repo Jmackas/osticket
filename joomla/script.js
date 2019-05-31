abf59cc0ce372873a2bd19e602e4a30cea8f5e7d
James Mackay <jamesrossmackay@gmail.com>
2019-05-31 14:42:04 +0800

    Status is sort of broken

--- joomla/script.js
+++ joomla/script.js
@@ -1,5 +1,38 @@
 adminInterface();
 
 function adminInterface() {
-    document.querySelector('input[datavalue="7"]').value = "Hello";
+    // Insert buttons
+  //  document.querySelector("body").insertAdjacentHTML('afterbegin', '<button onclick="actionFields();">Responded</button>');
+
+    checkStatus();
+
+}
+
+function actionFields() {
+    // Overwrite the status field
+    document.querySelector('input[datavalue="6"]').value = "Completed";
+
+    
+    // Detect the user
+    let user = document.querySelector(".login-greeting").innerText;
+    if (user == "Hi Super User,") {
+        // Overwrite the email field
+        document.querySelector('input[datavalue="7"]').value = "james.mackay@eit.edu.au";
+    }
+
+}
+
+
+// Filter rows based on status
+function checkStatus() {
+    let ticketStatus = document.querySelectorAll("*").innerHTML;
+
+    console.log(ticketStatus[2]);
+
+   /* for(i = 0; i < status.length; i++) {
+        if(status[i] == "Completed") {
+            console.log("worked!");
+        }
+    } */
+   
 }
