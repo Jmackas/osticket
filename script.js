4d1345d15f428e89833e08a7bd4d54ee625fd189
James Mackay <jamesrossmackay@gmail.com>
2019-06-05 15:27:18 +0800

    Minor progress with element removal

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 // Invoke functions
+removeElements();
 selectTopic();
+
+// Remove unecessary page elements
+function removeElements() {
+    console.log("hello world!");
+}
 
 // Detect where the user came from to filter topics
 function selectTopic() {
-  // If user came from the EIT SharePoint site
-  if (document.referrer == "https://eittraining.sharepoint.com/sites/staff-eit") {
-    document.getElementById("topicId").selectedIndex = "1";
-  }
+    // If user came from the EIT SharePoint site
+    if (document.referrer == "https://eittraining.sharepoint.com/sites/staff-eit") {
+        document.getElementById("topicId").selectedIndex = "1";
+    }
+    // If user came from Remote Labs
+    else if (document.referrer == "https://lab.electromeet.com/support") {
+        document.getElementById("topicId").selectedIndex = "2";
+    }
 
-  // If user came from Remote Labs
-  else if (document.referrer == "https://lab.electromeet.com/support") {
-    document.getElementById("topicId").selectedIndex = "2";
-  }
+    document.querySelector("body").insertAdjacentHTML('afterend', '<link href="https://jmackas.github.io/osticket/styles.css" rel="stylesheet">');
 
-  
- document.querySelector("body").insertAdjacentHTML('afterend', '<link href="https://jmackas.github.io/osticket/styles.css" rel="stylesheet">');
-
-  // Refresh the module with the updated content (Taken from an in-built script)
-  document.getElementById("topicId").onchange("click", function () {
-    var data = $(":input[name]", "#dynamic-form").serialize();
-    $.ajax("ajax.php/form/help-topic/" + this.value, {
-      data: data,
-      dataType: "json",
-      success: function (a) {
-        $("#dynamic-form").empty().append(a.html),
-          $(document.head).append(a.media)
-      }
+    // Refresh the module with the updated content (Taken from an in-built script)
+    document.getElementById("topicId").onchange("click", function() {
+        var data = $(":input[name]", "#dynamic-form").serialize();
+        $.ajax("ajax.php/form/help-topic/" + this.value, {
+            data: data,
+            dataType: "json",
+            success: function(a) {
+                $("#dynamic-form").empty().append(a.html),
+                $(document.head).append(a.media)
+            }
+        });
     });
-  });
 }
